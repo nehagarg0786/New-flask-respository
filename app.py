8d117725fb33278037fa76dab6f9eecb313dbe4a
Neha <nehagargofficial123@gmail.com>
2021-03-09 21:20:06 +0000

    added pyodbc

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import logging
 import urllib.parse
 
 app = Flask(__name__)
-params=urllib.parse.quote_plus("Driver={SQL Server};Server=tcp:dbserver-2.database.windows.net,1433;Database=db-create-1;Uid=nehaadmin;Pwd=Welcome@123;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;")
+params=urllib.parse.quote_plus("Driver={ODBC Driver 17 for SQL Server};Server=tcp:dbserver-2.database.windows.net,1433;Database=db-create-1;Uid=nehaadmin;Pwd=Welcome@123;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;")
 app.config['SQLALCHEMY_DATABASE_URI'] = "mssql+pyodbc:///?odbc_connect=%s" % params
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 app.config['SECRET_KEY'] = 'super secret key'
